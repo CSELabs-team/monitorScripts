79fc5041442cd289f432975b96bb726cc511b088
Anirudh Ramachandran <anirudhr1989@gmail.com>
2014-08-25 13:29:02 -0400

    Practice server health monitoring script

---
+++ healthMon.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+
+#1. Write a python script to detect system disk, cpu, memory usage in Linux OS.
+#2. When it reachs 90% warn, 95% critical, and the script will send an email alert to cselabs-team-group mailing list.
